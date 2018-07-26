25af3046f98a45dbab2057703cdc094eda5d091f
Nicholas Spyrison <spyrison@gmail.com>
2018-07-26 13:15:28 +1000

    del files

--- lab01/ns_lab01.r
+++
@@ -1,9 +0,0 @@
-### ns ETC 3555 lab1
-
-#https://github.com/camroach87/SML2018/tree/master/labs/lab01
-#data: archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/
-
-library(tidyverse)
-
-red <- read_csv2(file="./lab01/data/winequality-red.csv")
-wht <- read_csv2(file="./lab01/data/winequality-white.csv")
